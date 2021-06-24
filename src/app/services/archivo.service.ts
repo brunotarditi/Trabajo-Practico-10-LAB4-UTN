726be7e1139ae21956860a35ff1beba5939ad6e1
Bruno Tarditi <brunotarditi@hotmail.com>
2021-06-24 19:27:43 -0300

    Arreglando httpErrorResponse 200 en archivo service

--- src/app/services/archivo.service.ts
+++ src/app/services/archivo.service.ts
@@ -12,8 +12,11 @@ export class ArchivoService {
 
     //Upload del archivo
     upload(imagen: File): Observable<any> {
+      const requestOptions : Object = {
+        responseType: 'blob'
+      }
       const formData = new FormData();
       formData.append('file', imagen);
-      return this.http.post<any>(this.url + 'upload', formData);
+      return this.http.post<any>(this.url + 'upload', formData, requestOptions);
     }
 }
